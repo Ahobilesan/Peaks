d65f63a52a6697101ef88a673f0ea905b5df55e8
Ahobilesan <ahobilesan.gurumurthy@gmail.com>
2021-06-26 22:53:22 +0530

    source file base commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="peaks">
+        <nav className="navbar"></nav>
+
+        <footer className="footer"></footer>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+
+  }
+}
+
+export default App;
---
+++ src/backend/interface.ts
@@ -0,0 +1,7 @@
+export interface STORIES {
+
+}
+
+export interface STORY {
+
+}
