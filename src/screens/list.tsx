d8991062abf924a382be8c0741e675b7decf31d5
Ahobilesan <ahobilesan.gurumurthy@gmail.com>
2021-06-29 02:02:14 +0530

    list screen updated

--- src/screens/list.tsx
+++ src/screens/list.tsx
@@ -4,47 +4,43 @@ import Card from "../components/card"
 import Loader from "../components/loading"
 import "./screen.scss"
 
-const defaultState = { view: "" }
-class List extends React.Component<{ loading: boolean, stories: any[] }> {
-
-    constructor(props: any) {
-        super(props)
-        this.setState({ ...defaultState })
-    }
+class List extends React.Component<{ global?: any, loading: boolean, stories: any[] }> {
 
     render() {
         let main: any
         let group: any = []
+
         return <div className="list">
             {!this.props.loading &&
                 <div className="cards">
                     {this.props.stories && this.props.stories.map((story: STORY, id) => {
-                        if (id > 4) {
+                        if (this.props.global.screen.width <= 1200) {
                             return <Card key={id} idx={id} story={story} />
-                        }
-
-                        if (id == 0) {
-                            main = <Card key={id} idx={id} story={story} />
-                            return
-                        }
-
-                        if (id >= 1 || id < 4) {
-                            group.push(<Card key={id} idx={id} story={story} />)
-
-                            if (group.length == 4) {
-                                return <div key="first" className="first">{main} <div className="group">{group}</div></div>
+                        } else {
+                            if (id > 4) {
+                                return <Card key={id} idx={id} story={story} />
                             }
-                            return
+
+                            if (id === 0) {
+                                main = <Card key={id} idx={id} story={story} />
+                                return undefined
+                            }
+
+                            if (id >= 1 || id < 4) {
+                                group.push(<Card key={id} idx={id} story={story} />)
+
+                                if (group.length === 4) {
+                                    return <div key="first" className="first">{main} <div className="group">{group}</div></div>
+                                }
+                                return undefined
+                            }
                         }
+                        return undefined
                     })}
                 </div>
             }
             {this.props.loading && <Loader detail="Loading Stories" />}
         </div>
-    }
-
-    componentDidMount() {
-
     }
 }
 
