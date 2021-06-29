819e497191cc1fe734be5712e1019d35710593e5
Ahobilesan <ahobilesan.gurumurthy@gmail.com>
2021-06-30 01:06:36 +0530

    List and card updated for Bookmark

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,16 +4,9 @@ import { moment } from "../util"
 
 import "./component.scss"
 
-const defaultState = {}
-class Card extends React.Component<{ story: STORY, idx?: number }> {
-
-    constructor(props: any) {
-        super(props)
-        this.setState({ ...defaultState })
-    }
-
+class Card extends React.Component<{ story: STORY, idx?: number, bookmark?: boolean, onClick: Function }> {
     render() {
-        return <div className={`card ${this.props.idx === 0 ? "large" : this.props.idx! <= 4 ? "mini" : "small"}`} >
+        return <div onClick={this.handleClick.bind(this)} className={`card ${this.props.bookmark ? "small" : this.props.idx === 0 ? "large" : this.props.idx! <= 4 ? "mini" : "small"}`} >
             <img alt="" src={this.props.story.image.url} style={{ backgroundImage: `url(${this.props.story.image.thumbnail})` }} />
             <div className="content">
                 <div className="header">{this.props.story.title}</div>
@@ -23,6 +16,12 @@ class Card extends React.Component<{ story: STORY, idx?: number }> {
                 {this.props.story.title.length < 90 && <div className="description">{this.props.story.description.substring(0, 80)}...</div>}
             </div>
         </div>
+    }
+
+    handleClick() {
+        if (this.props.onClick) {
+            this.props.onClick(this.props.story, this.props.idx)
+        }
     }
 }
 
