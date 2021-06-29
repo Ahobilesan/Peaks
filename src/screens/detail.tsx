6ab5a66306fae041acb84666cfe2d7eeea1c413a
Ahobilesan <ahobilesan.gurumurthy@gmail.com>
2021-06-30 01:06:00 +0530

    detail screen created

--- src/screens/detail.tsx
+++ src/screens/detail.tsx
@@ -1,8 +1,34 @@
 import React from "react";
+import { moment } from "../util";
+import { STORY } from "../backend/interface";
+import Loader from "../components/loading"
+import "./screen.scss"
 
-class Detail extends React.Component {
+class Detail extends React.Component<{ global?: any, story: STORY }> {
+    state = { loading: true }
     render() {
-        return <div>Detail Screen</div>
+        return <div className="detail">
+            {this.state.loading && <Loader detail="Loading" />}
+
+            {!this.state.loading && <div className="content-wrapper">
+                <div className="meta">
+                    {/* <span className="provider">{this.props.story.provider.name}</span>  */}
+                    <span className="date">{moment(this.props.story.datePublished).fromNow()}</span>
+                </div>
+                <h1>{this.props.story.title}</h1>
+                <h3>{this.props.story.description}</h3>
+                <article>{this.props.story.body}</article>
+            </div>}
+            {!this.state.loading && <div className="image-wrapper" style={{ backgroundImage: this.props.story.image.thumbnail }}>
+                <img src={this.props.story.image.url} />
+            </div>}
+        </div>
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ loading: false })
+        }, 500);
     }
 }
 
